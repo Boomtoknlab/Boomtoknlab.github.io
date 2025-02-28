721b52fd47fb386a21189080eff3297788e3be30
BOOM TOKEN <marinachris112@outlook.com>
2025-02-28 07:43:58 +0100

    Create JavaScript.js
    Signed-off-by: BOOM TOKEN <marinachris112@outlook.com>

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('interactive-button');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
